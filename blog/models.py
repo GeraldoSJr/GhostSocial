34688f1af29605522873cbdd0c2f2a5debbfbec8
Geraldo157 <gsmecanica76@gmail.com>
2023-03-26 12:53:39 -0300

    Criação da aplicação de cadastro

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    datePost = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User ,default = "Excluido", on_delete=models.SET_DEFAULT)
+
+    def __str__(self):
+        return self.title
+
--- django_project/settings.py
+++ django_project/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
