0b9ae27fa8876fae1f880e044ceaa0dbc5f3b107
Geraldo157 <gsmecanica76@gmail.com>
2023-04-04 16:06:23 -0300

    preparação para deploy

--- django_project/settings.py
+++ django_project/settings.py
@@ -131,4 +131,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'statiscfiles')
