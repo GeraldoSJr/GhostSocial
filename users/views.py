7151aabd7128512b17da1d7d46c5775900bcb1ff
Geraldo157 <gsmecanica76@gmail.com>
2023-05-26 14:18:49 -0300

    Finalizing messages exibition and the create post template

--- users/views.py
+++ users/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.models import User
